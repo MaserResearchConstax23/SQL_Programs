ae636d2ded46877db9334602656ec323e1c9b87d
Gaudeor Rudmin <rudmingb@dukes.jmu.edu>
2023-05-25 12:54:45 -0400

    create proof of dupes in nodupes, finish final task

--- create_no_masers_from_survey_and_maserlist_final.sql
+++ create_no_masers_from_survey_and_maserlist_final.sql
@@ -1,28 +1,16 @@
 SELECT *
 
-FROM AllSurveyed2021_NoDup survey
+FROM AllSurveyed2021_NoDup S
 
 WHERE 
 
-survey.ra IN (
-    SELECT  survey.ra,
-            survey.dec, 
-            known_masers.RA_(J2000),
-            known_masers.Dec_(J2000)
-    FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
-    WHERE
-        survey.ra IN (
-            SELECT survey.ra
-            FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
-            WHERE known_masers.RA_(J2000) BETWEEN survey.ra - 5/3600 and survey.ra + 5/3600
-        ) AND
-        survey.dec IN (
-            SELECT survey.dec
-            FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
-            WHERE known_masers.Dec_(J2000) BETWEEN survey.dec - 5/3600 and survey.dec + 5/3600
-        ) AND
-        (5/3600) > ( SQRT((POWER( ( ( (survey.ra * (PI()/180)) - (known_masers.RA_(J2000) * (PI()/180)) )*( COS( (survey.dec * (PI()/180)) )) ),2) )+(POWER(( (survey.dec * (PI()/180)) - (known_masers.Dec_(J2000) * (PI()/180)) ),2))) ) * (180/PI())
+    S.ra NOT IN (
+        SELECT S.ra
+        FROM AllSurveyed2021_NoDup S, MCPMaser2020 M
+        WHERE
+        -- Small angular distance approximation
+        (5.0/3600.0) > ( SQRT((POWER( ( ( (S.ra * (PI()/180.0)) - (M.[RA_(J2000)] * (PI()/180.0)) )*( COS( (S.dec * (PI()/180.0)) )) ),2) )+(POWER(( (S.dec * (PI()/180.0)) - (M.[Dec_(J2000)] * (PI()/180.0)) ),2))) ) * (180.0/PI())
+    )
 
-)
 
 INTO MyDB.GBT_no_maser_no_duplicates
---
+++ proof_of_dups_in_no_dups.sql
@@ -0,0 +1,13 @@
+-- to reproduce, copy
+-- ConstantinResearchGroup.WilliamStJohn.AllSurveyed2021_NoDup as
+-- AllSurveyed2021_NoDup and ConstantinResearchGroup.CameronKelahan.MCPMaser2020
+-- as MCPMaser2020
+SELECT S.ra AS NODUP_RA, S.dec as NODUP_DEC, S.source as NODUP_NAME, M.[RA_(J2000)] AS MASER_RA, M.[Dec_(J2000)] AS MASER_DEC, M.Source_Name AS MASER_NAME
+FROM    ConstantinResearchGroup.WilliamStJohn.AllSurveyed2021_NoDup S, 
+        ConstantinResearchGroup.CameronKelahan.MCPMaser2020 M
+WHERE
+-- Small angular distance approximation
+(5.0/3600.0) > ( SQRT((POWER( ( ( (S.ra * (PI()/180.0)) - (M.[RA_(J2000)] * (PI()/180.0)) )*( COS( (S.dec * (PI()/180.0)) )) ),2) )+(POWER(( (S.dec * (PI()/180.0)) - (M.[Dec_(J2000)] * (PI()/180.0)) ),2))) ) * (180.0/PI())
+
+-- you will see obvious duplicates from inside NoDup
+-- one obvious example is NGC17, in NoDup as NGC0017 and NGC17
