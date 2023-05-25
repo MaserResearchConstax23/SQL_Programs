3af7aa787317882de285e6905c6b9b887974b10a
Gaudeor Rudmin <rudmingb@dukes.jmu.edu>
2023-05-25 12:54:33 -0400

    Complete first sql task, add proof that no_dups needs further refinements

--- create_no_masers_from_survey_and_maserlist_planning.sql
+++ create_no_masers_from_survey_and_maserlist_planning.sql
@@ -69,3 +69,50 @@ INTO MyDB.GBT_no_maser_no_duplicates
 --     FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
 --     WHERE known_masers.Dec_(J2000) BETWEEN survey.dec - 5/3600 and survey.dec + 5/3600
 -- )
+
+
+SELECT *
+
+FROM AllSurveyed2021_NoDup survey
+
+WHERE 
+
+survey.ra IN (
+    SELECT  survey.ra
+    FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
+    WHERE
+        survey.ra IN (
+            SELECT survey.ra
+            FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
+            WHERE 
+            POWER(known_masers.[RA_(J2000)] -survey.ra,2) <= POWER(5/3600,2) AND
+            POWER(known_masers.[Dec_(J2000)] - survey.dec,2) <= POWER(5/3600,2)
+            -- known_masers.[RA_(J2000)] BETWEEN survey.ra - 5/3600 and survey.ra + 5/3600 AND
+            -- known_masers.[Dec_(J2000)] BETWEEN survey.dec - 5/3600 and survey.dec + 5/3600 AND
+            (5/3600) > ( SQRT((POWER( ( ( (survey.ra * (PI()/180)) - (known_masers.[RA_(J2000)] * (PI()/180)) )*( COS( (survey.dec * (PI()/180)) )) ),2) )+(POWER(( (survey.dec * (PI()/180)) - (known_masers.[Dec_(J2000)] * (PI()/180)) ),2))) ) * (180/PI())
+
+        ) AND
+        survey.dec IN (
+            SELECT survey.dec
+            FROM AllSurveyed2021_NoDup survey, MCPMaser2020 known_masers
+            WHERE known_masers.[Dec_(J2000)] BETWEEN survey.dec - 5/3600 and survey.dec + 5/3600
+        ) AND
+        (5/3600) > ( SQRT((POWER( ( ( (survey.ra * (PI()/180)) - (known_masers.[RA_(J2000)] * (PI()/180)) )*( COS( (survey.dec * (PI()/180)) )) ),2) )+(POWER(( (survey.dec * (PI()/180)) - (known_masers.[Dec_(J2000)] * (PI()/180)) ),2))) ) * (180/PI())
+
+)
+
+INTO MyDB.GBT_no_maser_no_duplicates
+
+
+
+
+
+
+SELECT s.source,s.ra,m.[RA_(J2000)] as Maser_ra
+FROM AllSurveyed2021_NoDup s, MCPMaser2020 m
+WHERE
+
+POWER(m.[RA_(J2000)] - s.ra,2) <= POWER(200.0/3600.0,2) AND
+POWER(m.[Dec_(J2000)] - s.dec,2) <= POWER(200.0/3600.0,2)
+--known_masers.[RA_(J2000)] BETWEEN survey.ra - 1000/3600 and survey.ra + 1000/3600 AND
+--known_masers.[Dec_(J2000)] BETWEEN survey.dec - 1000/3600 and survey.dec + 1000/3600
